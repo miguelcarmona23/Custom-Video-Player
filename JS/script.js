b7ac97bfe86aa0c42126f066cc08b07ca728feab
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-07 14:52:28 -0500

    Added const progressBAr

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__field');
