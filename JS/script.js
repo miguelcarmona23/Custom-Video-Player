884a7825bd01f9877378fba1201a4386782b495d
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-07 16:12:04 -0500

    Added timeupdate to video.addEventListener

--- JS/script.js
+++ JS/script.js
@@ -34,6 +34,8 @@ function handleProgress() {
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
