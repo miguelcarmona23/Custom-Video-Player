9ee34820a660aa660e53d268709703ead0f28b17
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-07 14:54:49 -0500

    Added const skipButtons

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,4 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__field');
 const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip]');
