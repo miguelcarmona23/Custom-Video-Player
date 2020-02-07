afe8a50893d5e7bc7606557cd1a69ffde74146d6
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-07 14:55:30 -0500

    Added const ranges

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,4 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__field');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
